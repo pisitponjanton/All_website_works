696eefd924f98d0c19b5b434f851beba5bcb9175
pisitponjanton <pisitponjan223@gmail.com>
2024-12-01 17:14:20 +0700

    update

--- work/app/maincomponent/Contact.jsx
+++ work/app/maincomponent/Contact.jsx
@@ -2,40 +2,62 @@
 
 export default function Contact() {
   return (
-    <div className="w-screen h-[70vh] bg-[#1B1B33] mt-[50px] relative flex flex-col gap-[50px] overflow-hidden">
-      <div className="w-full flex justify-around items-center relative top-[40px]">
-        <div className="w-[580px] h-[220px] bg-namecon bg-cover bg-center bg-no-repea"></div>
-        <div className="flex flex-col justify-center relative right-[100px] text-[#fff] font-inter-bold gap-3">
-          <div className="text-[45px]">Navigation</div>
-          <div className=" flex flex-col gap-3">
-            <div className="text-[20px]">Home</div>
-            <div className="text-[20px]">About us</div>
-            <div className="text-[20px]">Contact</div>
+    <div className="w-screen pb-[50px] bg-[#1B1B33] flex justify-center items-center mt-[50px] font-inter-bold text-[#fff] pt-[100px] overflow-hidden
+    max-[1290px]:flex-col
+    max-[1290px]:pt-[50px]
+    max-[500px]:pt-[0px]
+    ">
+      <div className="w-[50%] h-full
+      max-[1290px]:w-full
+      max-[700px]:scale-[0.7]
+      max-[500px]:scale-[0.5]">
+        <div className="flex flex-col items-start px-[100px] 
+      max-[700px]:px-[20px]
+      max-[500px]:px-[0px]
+        ">
+          <div className="bg-namecon bg-no-repeat bg-cover bg-center w-[580px] h-[220px] relative
+          max-[500px]:scale-[0.9] max-[500px]:left-[-100px]"></div>
+
+          <div className="flex justify-center items-center gap-[30px] mt-[50px]">
+            <div className="bg-map bg-no-repeat bg-cover bg-center w-[70px] h-[70px]"></div>
+            <div className="text-[20px] flex flex-col">
+              <div>Street3720 PoteeSt.</div>
+              <div>City Baltimore State Maryland Postal</div>
+              <div> Code21225 Country United States</div>
+            </div>
           </div>
-        </div>
-      </div>
-      <div className="w-full flex justify-around relative items-center text-[#fff] font-inter-bold ">
-        <div className="flex justify-center items-center relative left-1 gap-[40px]">
-          <div className="bg-map bg-cover bg-center bg-no-repeat w-[70px] h-[70px]"></div>
-          <div className="w-[400px] text-[25px]">
-            Street3720 PoteeSt. City Baltimore State Maryland Postal Code21225
-            Country United States
-          </div>
-        </div>
-        <div className="flex relative justify-center items-center right-[-54px] gap-[30px]">
-          <div className="bg-mail bg-cover bg-center bg-no-repeat w-[60px] h-[60px]"></div>
-          <div className="text-[25px]">freshesdrink@hotmail.com</div>
-        </div>
-      </div>
-      <div className="w-full flex justify-around relative items-center text-[#fff] font-inter-bold">
-        <div className="flex justify-center items-center gap-[30px] relative left-[-90px]">
+
+          <div className="flex justify-center items-center gap-[30px] mt-[100px]">
             <div className="bg-tel bg-cover bg-center bg-no-repeat w-[60px] h-[60px]"></div>
             <div className="text-[25px]">(410) 355-2227</div>
+          </div>
         </div>
-        <div className="flex justify-center items-center gap-[30px] relative right-[-165px]">
+      </div>
+
+      <div className="w-[50%] h-full
+      max-[1290px]:w-full max-[1290px]:mt-[100px]
+      max-[700px]:scale-[0.7] max-[700px]:mt-[-50px]
+      max-[500px]:scale-[0.5] max-[500px]:mt-[-200px]">
+        <div className="flex flex-col items-start px-[100px]
+        max-[700px]:px-[20px]
+        max-[500px]:px-[0px]">
+          <div className="text-[#fff]">
+            <div className="text-[40px]">Navigation</div>
+            <div className="text-[20px] mt-[20px]">Home</div>
+            <div className="text-[20px] mt-[10px]">About us</div>
+            <div className="text-[20px] mt-[10px]">Contact</div>
+          </div>
+
+          <div className="flex justify-center items-center gap-[30px] mt-[100px]">
+            <div className="bg-mail bg-cover bg-center bg-no-repeat w-[60px] h-[60px]"></div>
+            <div className="text-[25px]">freshesdrink@hotmail.com</div>
+          </div>
+
+          <div className="flex justify-center items-center gap-[30px] mt-[100px]">
             <div className="bg-ig bg-cover bg-center bg-no-repeat w-[50px] h-[50px]"></div>
             <div className="bg-face bg-cover bg-center bg-no-repeat w-[50px] h-[50px]"></div>
             <div className="bg-youtube bg-cover bg-center bg-no-repeat w-[60px] h-[60px]"></div>
+          </div>
         </div>
       </div>
     </div>
