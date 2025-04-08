45b0c3877a3601059d8f619dd7ad473a2624209b
pisitponjanton <pisitponjan223@gmail.com>
2025-04-08 15:33:22 +0700

    update Hero3

--- WebM_Te/latcostllostcat/component/Hero3.jsx
+++ WebM_Te/latcostllostcat/component/Hero3.jsx
@@ -6,7 +6,7 @@ export default function Hero3() {
   return (
     <div className="w-screen flex flex-col justify-center items-center z-[1] relative mt-[12vw]">
       <div className="bg-[url('/image/Hero3/bgfull.png')] duration-500 ease-in-out hover:scale-105 bg-cover bg-center w-[85vw] h-[40.5vw] absolute top-[-20vw]"></div>
-      <div className="w-screen h-[20vw] bg-[#1997AD] relative mt-[25vw] max-[441px]:h-[62vw]">
+      <div className="w-screen h-[20vw] bg-[#1997AD] relative mt-[25vw]">
         <div className=" absolute w-[15vw] h-[16vw] bg-[url('/image/Hero3/cat.png')] bg-cover bg-center left-[4vw] top-[2vw]"></div>
         <div className=" relative w-[35vw] right-[-23vw] top-[3vw] text-[2.2vw] text-[#0E4F5A]">
           If you{"'"}d like to give a special cat a loving home or support our
